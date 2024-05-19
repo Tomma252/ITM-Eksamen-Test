39c612fc7fd020e40b7bb9703089a940fd0b9b2c
Tomma252 <thomasb95@hotmail.com>
2024-05-19 16:17:01 +0200

    Home.jsx oppdatering
    Lagt til kode i Home.jsx for å vise main pokémons og alle types.

--- eksamen/src/Components/Home.jsx
+++ eksamen/src/Components/Home.jsx
@@ -1,19 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
+    const [pokemonList, setPokemonList] = useState([]);
+    const [pokemonTypes, setPokemonTypes] = useState([]);
+
+    useEffect(() => {
+        const fetchPokemonList = async () => {
+            try {
+                const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=9');
+                const results = response.data.results;
+
+                const detailedPokemonList = await Promise.all(
+                    results.map(async (pokemon) => {
+                        const pokemonDetails = await axios.get(pokemon.url);
+                        return pokemonDetails.data;
+                    })
+                );
+
+                setPokemonList(detailedPokemonList);
+            } catch (error) {
+                console.error("Error fetching Pokémon list.", error);
+            }
+        };
+
+        const fetchPokemonTypes = async () => {
+            try {
+                const response = await axios.get('https://pokeapi.co/api/v2/type');
+                setPokemonTypes(response.data.results);
+            } catch (error) {
+                console.error("Error fetching Pokémon types.")
+            }
+        }
+
+        fetchPokemonList();
+        fetchPokemonTypes();
+    }, []);
+
     return (
-        <article>
-            <h1></h1>
+        <div>
             <nav>
                 <ul>
-                    <li><Link to="/teams">Teams</Link></li>
-                    <li><Link to="/type">Type</Link></li>
-                    <li><Link to="/search">Search</Link></li>
+                    <li><Link to="/teams"></Link></li>
+                    <li><Link to="/type"></Link></li>
+                    <li><Link to="/search"></Link></li>
                 </ul>
             </nav>
-        </article>
-    );
+            <div>
+                <h2>MAIN POKÉMONS</h2>
+                <div>
+                    {pokemonList.map((pokemon, index) => (
+                        <span key={index}>
+                            <p>{pokemon.name}</p>
+                        </span>
+                    ))}
+                </div>
+                <span>
+                    <h3>TYPES</h3>
+                    <ul>
+                        {pokemonTypes.map((type, index) => (
+                            <li key={index}>{type.name}</li>
+                        ))}
+                    </ul>
+                </span>
+            </div>
+        </div>
+    )
 };
 
 export default Home;
