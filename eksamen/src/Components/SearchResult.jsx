6a18ec7ad58fb552b66944ebb08268a116651921
Tomma252 <thomasb95@hotmail.com>
2024-05-20 19:36:33 +0200

    SearchResult API
    Fikset API kall for SearchResult.jsx

--- eksamen/src/Components/SearchResult.jsx
+++ eksamen/src/Components/SearchResult.jsx
@@ -25,7 +25,7 @@ const SearchResult = () => {
             const englishEntry = effectEntries.find(entry => entry.language.name === 'en');
             return englishEntry ? englishEntry.effect : 'No description available.';
         } catch (error) {
-            console.error('Error fetching ability description.');
+            console.error('Error fetching ability description.', error);
             return 'No description available.';
         }
     };
@@ -37,13 +37,13 @@ const SearchResult = () => {
     };
 
     useEffect(() => {
-        if (results.length > 0) {
+        if (results.length > 0 && results[0].abilities && !results[0].abilities[0].description) {
             const fetchDescriptions = async () => {
                 const abilities = results[0].abilities;
                 const descriptions = await Promise.all(
                     abilities.map(async (ability) => ({
-                            name: ability.ability.name,
-                            description: await fetchAbilityDescription(ability.ability.url)
+                        name: ability.ability.name,
+                        description: await fetchAbilityDescription(ability.ability.url)
                     }))
                 );
                 setResults([{ ...results[0], abilities: descriptions }]);
@@ -55,30 +55,42 @@ const SearchResult = () => {
     return (
         <article>
             <h2>Search for a Pokémon</h2>
-            <input type="text" value={search} onChange={(e) => setSearch(e.target.value)}/>
+            <input
+                type="text"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                onKeyDown={(e) => e.key === 'Enter' && handleSearch()}
+            />
             <button onClick={handleSearch}>Search</button>
             {error && <p>{error}</p>}
             {results.length > 0 && results.map((pokemon, index) => (
                 <div key={index}>
-                    <h3>{pokemon.name}</h3>
-                    <img src={pokemon.sprites.front_default} alt={pokemon.name}/>
-                    <h3>TYPE(S)<br></br>
-                        {pokemon.types.map((type, index) => <span key={index}>{type.type.name}{index < pokemon.types.length - 1 ? ', ' : ''}</span>)}
-                    </h3>
-                    <h3>ABILITIES <br></br>
-                        {pokemon.abilities.map((ability, index) => (
-                            <p key={index}>{ability.name}:<br></br> {ability.description}</p>
+                    <h3>{pokemon.name.toUpperCase()}</h3>
+                    <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+                    <h3>TYPE(S)</h3>
+                    <p>
+                        {pokemon.types.map((type, index) => (
+                            <span key={index}>
+                                {type.type.name}{index < pokemon.types.length - 1 ? ', ' : ''}
+                            </span>
                         ))}
-                    </h3>
+                    </p>
+                    <h3>ABILITIES</h3>
+                    {pokemon.abilities.map((ability, index) => (
+                        <section key={index}>
+                            <h4>{ability.name}</h4>
+                            <p>{ability.description}</p>
+                        </section>
+                    ))}
                     <h3>STATS</h3>
                     <ul>
                         {pokemon.stats.map((stat, index) => (
                             <li key={index}>
                                 {stat.stat.name}: {stat.base_stat}
                             </li>
-                    ))}
-                </ul>
-            </div>
+                        ))}
+                    </ul>
+                </div>
             ))}
         </article>
     );
